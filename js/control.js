80e2fd03343d5d7dacf10061a35873679f590267
Brian Margolis <Brian.M.Margolis@gmail.com>
2017-02-20 18:28:20 -0600

    Added initial login page, profile page

--- js/control.js
+++ js/control.js
@@ -2,7 +2,7 @@
  * Created by brianmargolis on 2/9/17.
  */
 function main() {
-    $('.dropdown-inverse li > a').click(function(e){
+    $('.dropdown-inverse li > a').click(function (e) {
         $('.status').text(this.innerHTML);
     });
 }
