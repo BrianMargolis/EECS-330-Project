23261565870fe0b78a7c3abb1ac480ac83ff3550
Maggie <margaretlou2019@u.northwestern.edu>
2017-02-27 23:04:01 -0600

    fix button

--- php/time_selection.php
+++ php/time_selection.php
@@ -13,14 +13,14 @@ if (isset($_GET["period"])) {
         </div>
         <div class="col-md-2" id="time_selection">
             <div class="btn-group">
-                <button class="btn btn-primary status">Past <?php echo $period ?></button>
-                <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                <button class="btn btn-primary status" id = "time_button">Past <?php echo $period ?></button>
+                <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" id = "time_button">
                     <span class="caret" style = "color: rgb(219, 221, 224)"></span>
                 </button>
                 <ul class="dropdown-menu dropdown-inverse">
-                    <li><a href='?period=day' onclick="updatePeriod('day')">Past day</a></li>
-                    <li><a href='?period=week' onclick="updatePeriod('week')">Past week</a></li>
-                    <li><a href='?period=month' onclick="updatePeriod('month')">Past month</a></li>
+                    <li><a href='?period=day' onclick="updatePeriod('day')">PAST DAY</a></li>
+                    <li><a href='?period=week' onclick="updatePeriod('week')">PAST WEEK</a></li>
+                    <li><a href='?period=month' onclick="updatePeriod('month')">PAST MONTH</a></li>
                 </ul>
             </div>
         </div>
