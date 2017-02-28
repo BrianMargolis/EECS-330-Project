27892d7573817f96b507e3bff29638d842729c52
Maggie <margaretlou2019@u.northwestern.edu>
2017-02-27 21:39:42 -0600

    edit header

--- php/header.php
+++ php/header.php
@@ -1,5 +1,37 @@
+<?php
+
+if (isset($_GET["period"])) {
+    $period = $_GET["period"];
+} else {
+    $period = 'week';
+}
+
+function isActive($page, $active)
+{
+    if ($active == $page) {
+        echo "class = active";
+    }
+}
+
+?>
+
 <div class="header">
     <div class="container-fluid">
+        <div class = "row">
+            <div class = "col-md-2">
+                <a href="../index.php"><img src="../images/Logo_Rough.png" alt="soundscape"/></a>
+            </div>
+            <div class = "col-md-10">
+                <ul>
+                    <li><a href="../index.php?period=<?php echo $period ?>" <?php isActive('dashboard', $active) ?>>DASHBOARD</a></li>
+                    <li><a href="../favorites.php?period=<?php echo $period ?>" <?php isActive('favorites', $active) ?>>FAVORITES</a></li>
+                    <li><a href="../recent_stats.php?period=<?php echo $period ?>" <?php isActive('recent_stats', $active) ?>>Recent Stats</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+
         <div class="row">
             <div class="col-md-2" id="accountInfo">
                 <a href="../profile.php">
