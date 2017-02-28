294bd4d393895e635df5a3446951c4d0b7ea11f7
Brian Margolis <Brian.M.Margolis@gmail.com>
2017-02-28 12:01:29 -0600

    Password input formats as dots

--- login.php
+++ login.php
@@ -4,24 +4,25 @@
     <?php include "php/head.php" ?>
     <title>Login</title>
 </head>
-<body onload="main()" style = "background-color:rgba(107, 107, 107, 1)">
+<body onload="main()" style="background-color:rgba(107, 107, 107, 1)">
 
 <div class="container-fluid">
-    <div id = "login_box" >
-        <div class = "row">
-            <img src="images/Logo_Rough.png" alt="soundscape" style = "margin-left: 6em; margin-top: 2.5em; margin-bottom: 0.5em"/>
+    <div id="login_box">
+        <div class="row">
+            <img src="images/Logo_Rough.png" alt="soundscape"
+                 style="margin-left: 6em; margin-top: 2.5em; margin-bottom: 0.5em"/>
         </div>
-        <div class = "row">
-            <input id = "login_field" type = "text" placeholder="Username" >
+        <div class="row">
+            <input id="login_field" type="text" placeholder="Username">
             <br>
-            <input type = "text" placeholder = "Password" id = "login_field">
+            <input type="password" placeholder="Password" id="login_field">
             <br>
-            <a href="index.php"><input class="button" id = "login_button" type="submit" value="Sign In"></a>
+            <a href="index.php"><input class="button" id="login_button" type="submit" value="Sign In"></a>
             <br>
             <br>
             <a href="signup.php" style="color: rgba(107,107,107,1); margin-left: 20%;">Need an account? Sign up here</a>
             <br>
-            <a href="forgot_password.php" style="color: rgba(107,107,107,1); margin-left: 30%;" >Forgot password?</a>
+            <a href="forgot_password.php" style="color: rgba(107,107,107,1); margin-left: 30%;">Forgot password?</a>
         </div>
     </div>
 </div>
--- signup.php
+++ signup.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <?php include "php/head.php"?>
+    <?php include "php/head.php" ?>
     <title>Sign Up</title>
 </head>
-<body onload="main()" style = "background-color:rgba(107, 107, 107, 1)">
+<body onload="main()" style="background-color:rgba(107, 107, 107, 1)">
 
 <div class="container-fluid">
-    <div id = "login_box" style="margin-top: 3em">
-        <div class = "row">
-            <img src="images/Logo_Rough.png" alt="soundscape" style = "margin-left: 6em; margin-top: 2.5em; margin-bottom: 0.5em"/>
+    <div id="login_box" style="margin-top: 3em">
+        <div class="row">
+            <img src="images/Logo_Rough.png" alt="soundscape"
+                 style="margin-left: 6em; margin-top: 2.5em; margin-bottom: 0.5em"/>
         </div>
-        <div class = "row">
-            <input id = "login_field" type = "text" placeholder="Name" >
+        <div class="row">
+            <input id="login_field" type="text" placeholder="Name">
             <br>
-            <input type = "text" placeholder = "Username" id = "login_field">
+            <input type="text" placeholder="Username" id="login_field">
             <br>
-            <input type = "text" placeholder = "Password" id = "login_field">
+            <input type="password" placeholder="Password" id="login_field">
             <br>
-            <input type = "text" placeholder = "Email" id = "login_field">
-            <input type = "text" placeholder = "Birthday" id = "login_field">
-            <a href="index.php"><input class="button" id = "login_button" type="submit" value="Sign Up"></a>
+            <input type="text" placeholder="Email" id="login_field">
+            <input type="text" placeholder="Birthday" id="login_field">
+            <a href="index.php"><input class="button" id="login_button" type="submit" value="Sign Up"></a>
             <br>
             <a href="login.php" style="color: rgba(107,107,107,1); margin-left: 20%;">Already a member? Sign in here</a>
             <br>
